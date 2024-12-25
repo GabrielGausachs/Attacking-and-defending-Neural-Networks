133f0b95ff8e8623e3f5d9c10da7934e01bd07f3
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-12-25 10:57:00 +0100

    extracting tarfiles

--- Utils/dataloader.py
+++ Utils/dataloader.py
@@ -1,6 +1,7 @@
 import tarfile
 import os
 import torch
+import tarfile
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from Utils.logger import initialize_logger, get_logger
@@ -29,6 +30,10 @@ class CustomDataloader:
         logger.info("-" * 50)
         logger.info(f'Reading the data from {self.images_path}...')
 
+
+        # Extract files from tar folder - Only if you have the validation folder as .tar. DO IT ONLY ONCE!
+        #self.reading_tarfiles()
+
         # Create dataset
         logger.info("-" * 50)
         logger.info('Creating dataset...')
@@ -50,6 +55,16 @@ class CustomDataloader:
         logger.info(f"Val loader info: {dataloader_info}")
 
         return dataloader
+    
+
+    def reading_tarfiles(self):
+        logger.info("Extracting files from .tar")
+        self.images_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"ILSVRC2012_img_val.tar")
+        os.makedirs(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"ILSVRC2012_img_val"), exist_ok=True)
+        with tarfile.open(self.images_path, 'r:*') as tar:
+            tar.extractall(path=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"ILSVRC2012_img_val"))
+            logger.info(f"Extracted {self.images_path} to {os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"ILSVRC2012_img_val")}")
+        self.images_path = IMAGE_PATH
 
 
 
