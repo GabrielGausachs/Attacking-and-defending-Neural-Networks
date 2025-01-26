db52d67a80ac1037016850a3e2a5942786264fdf
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2025-01-26 10:41:11 +0100

    preparing to save adv images

--- Utils/config.py
+++ Utils/config.py
@@ -24,7 +24,7 @@ LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Logs")
 IMAGE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"ILSVRC2012_img_val")
 LABEL_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),"val/ILSVRC2012_validation_ground_truth.txt")
 RESULTS_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Results")
-ADV_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Adversarial_images")
+ADV_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "Adversarial_images")
 
 # -----------------------------------------
 # Parameters 
@@ -32,7 +32,7 @@ ADV_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Adversarial
 
 BATCH_SIZE_ATTACK = 16
 NUM_WORKERS = 5
-IMAGES_TO_TEST = 20
+IMAGES_TO_TEST = 15000
 EPSILON = 0.03
 STEPSIZE = 0.005
 NUM_ITERATIONS = 10
